1d32275262c2497a3c728e284769669b0ce252cb
Bruce Steedman <bruce.steedman@gmail.com>
2015-06-03 10:13:40 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+ruby "2.2.2"
+
+gem 'bundler', '1.10.2'
+gem 'rake', '10.4.2'
+gem 'sinatra', '1.4.5'
---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+# ENV['RACK_ENV'] ||= 'development'
+
+# require 'bundler'
+# Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+get '/' do
+  'Hello World'
+end
